532a3ffc4e5d8ec4116217d6c0e2e62610694c14
tofan07 <tofan0797@gmail.com>
2021-03-11 19:00:04 +0300

    refact getBookingData.js

--- src/js/modules/getBookingData.js
+++ src/js/modules/getBookingData.js
@@ -1,10 +1,12 @@
 const getBookingData = () => {
-  const calendar = document.querySelectorAll(".calendar");
-  const calendarMonth = document.querySelector(".calendar__month");
-  const calendarDays = document.querySelector(".calendar__days");
   const date = new Date();
+  const bookingForm = document.getElementById("bookingForm");
 
-  const renderCalendar = () => {
+  const renderCalendar = elem => {
+    const calendar = elem.querySelector(".calendar");
+    const calendarMonth = calendar.querySelector(".calendar__month");
+    const calendarDays = calendar.querySelector(".calendar__days");
+
     const month = date.getMonth();
     const year = date.getFullYear();
     const lastDay = new Date(
@@ -12,6 +14,7 @@ const getBookingData = () => {
       date.getMonth() + 1,
       0
     ).getDate();
+
     const months = [
       "Январь",
       "Февраль",
@@ -61,38 +64,46 @@ const getBookingData = () => {
       days += `<div class="calendar__day calendar__day--empty"></div>`;
       calendarDays.innerHTML = days;
     }
-  };
-  const eventListeners = elem => {
-    elem.addEventListener("click", event => {
-      const target = event.target;
 
-      if (target.closest(".calendar__control")) {
-        if (target.matches(".calendar__button-prev")) {
-          date.setMonth(date.getMonth() - 1);
-        }
-        if (target.matches(".calendar__button-next")) {
-          date.setMonth(date.getMonth() + 1);
-        }
-        renderCalendar();
+    calendar.style.display = "block";
+  };
+
+  const hideCalendar = () => {
+    const calendar = bookingForm.querySelectorAll(".calendar");
+    calendar.forEach(elem => {
+      if (elem.style.display === "block") {
+        elem.style.display = "none";
       }
     });
   };
 
-  calendar.forEach(elem => eventListeners(elem));
-
-  const bookingForm = document.getElementById("bookingForm");
-
-  bookingForm.addEventListener("click", event => {
+  document.addEventListener("click", event => {
     let target = event.target;
 
-    target = target.closest(".hero-form__input-group");
+    if (!target.closest(".calendar")) {
+      hideCalendar();
+    }
 
+    target = target.closest("#bookingForm");
     if (target) {
-      target.querySelector(".calendar").classList.add("calendar_active");
+      event.preventDefault();
+      target = event.target;
+
+      if (target.matches(".calendar__button-prev")) {
+        date.setMonth(date.getMonth() - 1);
+      }
+
+      if (target.matches(".calendar__button-next")) {
+        date.setMonth(date.getMonth() + 1);
+      }
+
+      target = target.closest(".hero-form__input-group");
+
+      if (target && target.querySelector("input[type=date]")) {
+        renderCalendar(target);
+      }
     }
   });
-
-  renderCalendar();
 };
 
 export default getBookingData;
