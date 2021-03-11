8cb1dd3b572ba77c4a3fe900b3e8b913f0ea375b
tofan07 <tofan0797@gmail.com>
2021-03-11 15:07:47 +0300

    HMR added , webpack final config is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import toggleBurgerMenu from "@modules/toggleBurgerMenu";
 import getBookingData from "@modules/getBookingData";
-import "@modules/babel";
+
 import "@sass/styles.sass";
 
 toggleBurgerMenu();
--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,11 @@ module.exports = {
     },
   },
   devServer: {
+    open: true,
     port: 3000,
+    hot: false,
   },
+  target: isProd ? "browserslist" : "web",
   devtool: isDevFunc(),
   plugins: [
     new HtmlWebpackPlugin({
